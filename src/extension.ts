bfbd63597bfa1444db58313d04d2898238a3e9fe
Dan Doozan <dandoozan@gmail.com>
2019-01-24 18:44:27 -0700

    Add support for multicursor

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,28 @@
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, TextDocument } from 'vscode';
 import {
     generateAst,
     getCurrentEditor,
     getTextOfFile,
     getLanguage,
-    getCursorLocation,
+    getCursor,
     addCommand,
     deleteBetweenBoundary,
     filterAst,
     isCursorInsideNode,
     getBoundaryExcludingBraces,
     isString,
+    getCursors,
+    createDeleteModification,
+    Modification,
+    makeModifications,
+    Boundary,
 } from './utils';
 import { Node } from '@babel/types';
 import { maxBy } from 'lodash';
 
 function findEnclosingStringBoundary(ast: Node, cursorLocation: number) {
+    let stringBoundary;
+
     //there could be several enclosing strings (if, for example, there's a string
     //expression inside a template literal (eg. `Outer string ${'Inner string'}`))
     const enclosingStringNodes = filterAst(
@@ -23,39 +30,43 @@ function findEnclosingStringBoundary(ast: Node, cursorLocation: number) {
         node => isString(node) && isCursorInsideNode(cursorLocation, node)
     );
 
-    //so get the most enclosing one by finding the one with the last "start" value
-    const mostEnclosingStringNode = maxBy(enclosingStringNodes, 'start');
+    if (enclosingStringNodes.length > 0) {
+        //so get the most enclosing one by finding the one with the last "start" value
+        const mostEnclosingStringNode = maxBy(enclosingStringNodes, 'start');
 
-    //finally, get the boundary (and exclude the " (quote symbols) around it)
-    const stringBoundary = getBoundaryExcludingBraces(mostEnclosingStringNode);
+        //finally, get the boundary (and exclude the " (quote symbols) around it)
+        stringBoundary = getBoundaryExcludingBraces(mostEnclosingStringNode);
+    }
 
     return stringBoundary;
 }
 
 async function deleteInnerString() {
+    await applyModificationFunctionToEnclosingStrings(createDeleteModification);
+}
+
+async function applyModificationFunctionToEnclosingStrings(modificationFunction: Function) {
     const editor = getCurrentEditor();
 
     if (editor) {
         const ast = generateAst(getTextOfFile(editor), getLanguage(editor));
         if (ast) {
-            const stringBoundary = findEnclosingStringBoundary(
-                ast,
-                getCursorLocation(editor)
+            const cursors = getCursors(editor);
+            const stringBoundaries = cursors.map(cursor =>
+                findEnclosingStringBoundary(ast, cursor)
+            );
+            const modifications = stringBoundaries.map(stringBoundary =>
+                modificationFunction(editor.document, stringBoundary)
             );
 
-            //remove the string
-            if (stringBoundary) {
-                await deleteBetweenBoundary(editor, stringBoundary);
-            }
+            //do all the modifications
+            await makeModifications(editor, modifications);
         }
     }
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
     addCommand('vks.deleteInnerString', deleteInnerString, context);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
