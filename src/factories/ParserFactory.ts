830459afed4e1f7273066e54472b89d71d459003
Dan Doozan <dandoozan@gmail.com>
2019-02-20 19:05:33 -0700

    Add typescript language

--- src/factories/ParserFactory.ts
+++ src/factories/ParserFactory.ts
@@ -1,9 +1,11 @@
 import JavaScriptParser from '../parsers/JavaScriptParser';
+import TypeScriptParser from '../parsers/TypeScriptParser';
 import JsonParser from '../parsers/JsonParser';
 import { TextEditor } from 'vscode';
 
 const PARSER_CLASSES = {
     javascript: JavaScriptParser,
+    typescript: TypeScriptParser,
     json: JsonParser,
 };
 
---
+++ src/parsers/TypeScriptParser.ts
@@ -0,0 +1,8 @@
+import { generateBabelAst } from '../utils';
+import JavaScriptParser from './JavaScriptParser';
+
+export default class TypeScriptParser extends JavaScriptParser {
+    generateAst(code: string) {
+        return generateBabelAst(code, true);
+    }
+}
