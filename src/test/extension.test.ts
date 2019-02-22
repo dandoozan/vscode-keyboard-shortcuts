e67aa9e8ed8b36cf4f51efeaf4d1ac51d5c6f7fc
Dan Doozan <dandoozan@gmail.com>
2019-02-21 19:56:24 -0700

    Change tests to reuse the same editor (rather than creating a new editor for each test)

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,19 +1,50 @@
 import { strictEqual } from 'assert';
-import { workspace, window, commands } from 'vscode';
+import {
+    workspace,
+    window,
+    commands,
+    TextEditor,
+    TextEditorEdit,
+    Range,
+} from 'vscode';
 import { executeCommand } from '../extension';
 import { setCursor, getSelectedText, readFromClipboard } from '../utils';
 import testCases from './testCases';
 
-async function runCommandInEditor(language, type, action, testCase) {
-    const { startingCode, cursorPosition } = testCase;
-    const doc = await workspace.openTextDocument({
-        content: startingCode,
-        language,
-    });
+const EDITORS = {};
 
-    //show the editor so that it's the "activeTextEditor"
-    const editor = await window.showTextDocument(doc);
+async function getEditorForLanguage(language: string) {
+    if (!EDITORS[language]) {
+        const doc = await workspace.openTextDocument({
+            content: '',
+            language,
+        });
+
+        //show the editor so that it's the "activeTextEditor"
+        const editor = await window.showTextDocument(doc);
+
+        //cache it in EDITORS
+        EDITORS[language] = editor;
+    }
+    return EDITORS[language];
+}
+
+async function setEditorText(editor: TextEditor, newText: string) {
+    const oldTextRange = new Range(
+        editor.document.positionAt(0),
+        editor.document.positionAt(editor.document.getText().length)
+    );
+    await editor.edit((editBuilder: TextEditorEdit) => {
+        editBuilder.replace(oldTextRange, newText);
+    });
+}
+
+async function runCommandInEditor(type: string, action: string, testCase, editor: TextEditor) {
+    const { startingCode, cursorPosition } = testCase;
+
+    await setEditorText(editor, startingCode);
     await setCursor(editor, cursorPosition);
+
     // await commands.executeCommand(`vks.${commandName}`); // <- i tried this
     // but it seems it is not awaiting for some reason
     await executeCommand.call({ action, type }, editor);
@@ -55,7 +86,6 @@ function checkClipboardContent(expectedClipboardContent) {
     strictEqual(readFromClipboard(), expectedClipboardContent);
 }
 
-
 for (const language in testCases) {
     describe(language, () => {
         for (const type in testCases[language]) {
@@ -68,11 +98,12 @@ for (const language in testCases) {
                         }
                         for (const testCase of obj.testCases) {
                             it(testCase.desc, async () => {
-                                const editor = await runCommandInEditor(
-                                    language,
+                                const editor = await getEditorForLanguage(language);
+                                await runCommandInEditor(
                                     type,
                                     action,
-                                    testCase
+                                    testCase,
+                                    editor
                                 );
                                 actionTests[action](testCase, editor);
                             });
