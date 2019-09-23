6729ac0d6ddda63dc86710b9576366c2b7aa0546
Dan Doozan <dandoozan@gmail.com>
2019-09-22 22:05:33 -0700

    Give notification to the user when the parser fails

--- src/extension.ts
+++ src/extension.ts
@@ -16,22 +16,30 @@ export async function executeCommand(editor: TextEditor) {
     //@ts-ignore (i'm ts-ignoring the line below because it complains about the "this")
     const { action, type } = this;
 
-    const code = getFileText(editor);
     const language = getLanguage(editor);
     const parser = ParserFactory.createParser(language, editor);
-    const cursors = getCursors(editor);
+    if (parser) {
+        const code = getFileText(editor);
+        const cursors = getCursors(editor);
 
-    //get the most enclosing node for each cursor
-    const nodes = cursors
-        .map(cursor => parser.getMostEnclosingNodeOfType(type, cursor, code))
-        .filter(item => item); //filter out the undefined ones (an item is undefined when a cursor is outside the item)
+        //get the most enclosing node for each cursor
+        const nodes = cursors
+            .map(cursor =>
+                parser.getMostEnclosingNodeOfType(type, cursor, code)
+            )
+            .filter(item => item); //filter out the undefined ones (an item is undefined when a cursor is outside the item)
 
-    const actionBoundaries = nodes.map(node => node.getActionBoundary(action));
+        const actionBoundaries = nodes.map(node =>
+            node.getActionBoundary(action)
+        );
 
-    if (!isEmpty(actionBoundaries)) {
-        await Actions[action](editor, actionBoundaries as Boundary[]);
+        if (!isEmpty(actionBoundaries)) {
+            await Actions[action](editor, actionBoundaries as Boundary[]);
+        } else {
+            notify(`No enclosing "${type}" found`);
+        }
     } else {
-        notify(`No enclosing "${type}" found`);
+        notify(`Failed to create parser for "${language}" language.`);
     }
 }
 
